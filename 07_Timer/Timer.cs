80312256d166cd85db29611b468752145f9f29dc
Pelle <Pelle@LAPTOP-T69PBCOE>
2016-09-20 09:33:25 +0200

    Add project files.

---
+++ 07_Timer/Timer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace _07_Timer
+{
+    internal class Timer
+    {  
+    
+        public int Seconds;   
+        public override string ToString()
+        {
+            TimeSpan time = TimeSpan.FromSeconds(Seconds);
+            string str = time.ToString(@"hh\:mm\:ss");
+            return str;
+        }
+    }
+}
